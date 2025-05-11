e770840ce6159a3668ee97a0b115ae29904cbfc6
Vlad-PyDev <frotermamal@gmail.com>
2025-05-11 18:37:18 +0200

    Update tokens.go

--- pkg/ast/tokens.go
+++ pkg/ast/tokens.go
@@ -5,53 +5,51 @@ import (
 	"regexp"
 )
 
-// структура для первоначального разбиения строки на токены
 type token struct {
-	t   string // тип токена
-	val string // значение токена
-}
-
-// работает на регулярках, проверяет, является символ оператором
-func typeCheck(symbol string) bool {
-	pattern := "[+\\-*/]"
-	r, err := regexp.Compile(pattern)
-	if err != nil {
-		log.Fatalf("./pkg/ast (func: typeCheck); regexp error occured: %s", err)
-	}
-
-	return r.MatchString(string(symbol))
+	t   string
+	val string
 }
 
 func tokens(str string) []*token {
-	tokens := make([]*token, 0)
+	tokenList := make([]*token, 0)
 
-	i := 0
-	for i < len(str) {
+	index := 0
+	for index < len(str) {
 		switch {
-		case typeCheck(string(str[i])): // если оператор
-			tokens = append(tokens, &token{t: operator, val: string(str[i])})
-			i++
+		case typeCheck(string(str[index])):
+			tokenList = append(tokenList, &token{t: operator, val: string(str[index])})
+			index++
 
-		case str[i] >= 48 && str[i] <= 57: // если число
-			tmp := ""
-			for i < len(str) && ((str[i] >= 48 && str[i] <= 57) || str[i] == 44 || str[i] == 46) {
-				tmp += string(str[i])
-				i++
+		case str[index] >= '0' && str[index] <= '9':
+			numberStr := ""
+			for index < len(str) && ((str[index] >= '0' && str[index] <= '9') || str[index] == ',' || str[index] == '.') {
+				numberStr += string(str[index])
+				index++
 			}
-			tokens = append(tokens, &token{t: operand, val: string(tmp)})
+			tokenList = append(tokenList, &token{t: operand, val: numberStr})
 
-		case str[i] == 40 || str[i] == 41: // если скобка
-			tp := openBracket
-			if str[i] == 41 {
-				tp = closeBracket
+		case str[index] == '(' || str[index] == ')':
+			bracketType := openBracket
+			if str[index] == ')' {
+				bracketType = closeBracket
 			}
-			tokens = append(tokens, &token{t: tp, val: string(str[i])})
-			i++
+			tokenList = append(tokenList, &token{t: bracketType, val: string(str[index])})
+			index++
 
 		default:
-			i++
+			index++
 		}
 	}
 
-	return tokens
+	return tokenList
+}
+
+func typeCheck(symbol string) bool {
+	pattern := "[+\\-*/]"
+	regex, err := regexp.Compile(pattern)
+	if err != nil {
+		log.Fatalf("./pkg/ast (func: typeCheck); regexp compilation failed: %s", err)
+	}
+
+	return regex.MatchString(symbol)
 }
