26456c075ccab8020244293ff76456586e403355
Vlad-PyDev <frotermamal@gmail.com>
2025-05-11 18:43:51 +0200

    Update worker.go

--- internal/agent/worker.go
+++ internal/agent/worker.go
@@ -9,38 +9,38 @@ import (
 	"github.com/Vlad-PyDev/AsyncCalculationWebService/internal/models"
 )
 
-func worker(cfg config.Config) {
-	for task := range tasksCh {
-		log.Printf("worker got expression with id %v", task.ID)
-		result, err := calculate(task.Arg1, task.Arg2, task.Type, cfg)
-
-		res := &models.Result{ID: task.ID, Result: result, Error: err}
-		resultsCh <- res
-		log.Printf("worker sent result %v with id %v", result, task.ID)
-	}
-}
-
-func calculate(a, b string, operator string, cfg config.Config) (float64, string) {
-	a_float, _ := strconv.ParseFloat(a, 64)
-	b_float, _ := strconv.ParseFloat(b, 64)
+func calculate(arg1, arg2, operator string, cfg config.Config) (float64, string) {
+	firstValue, _ := strconv.ParseFloat(arg1, 64)
+	secondValue, _ := strconv.ParseFloat(arg2, 64)
 
 	switch operator {
 	case "*":
 		time.Sleep(cfg.TimeMultiplication)
-		return a_float * b_float, ""
+		return firstValue * secondValue, ""
 	case "/":
 		time.Sleep(cfg.TimeDivision)
-		if b_float == 0 {
+		if secondValue == 0 {
 			return 0, "division by zero"
 		}
-		return a_float / b_float, ""
+		return firstValue / secondValue, ""
 	case "+":
 		time.Sleep(cfg.TimeAddition)
-		return a_float + b_float, ""
+		return firstValue + secondValue, ""
 	case "-":
 		time.Sleep(cfg.TimeSubtraction)
-		return a_float - b_float, ""
+		return firstValue - secondValue, ""
 	default:
 		return 0, ""
+	}
+}
+
+func worker(cfg config.Config) {
+	for task := range tasksCh {
+		log.Printf("Worker received task with ID %v", task.ID)
+		calcResult, calcError := calculate(task.Arg1, task.Arg2, task.Type, cfg)
+
+		taskResult := &models.Result{ID: task.ID, Result: calcResult, Error: calcError}
+		resultsCh <- taskResult
+		log.Printf("Worker sent result %v for task ID %v", calcResult, task.ID)
 	}
 }
